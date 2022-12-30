c72497e38b05dda587fe2e067e1055f4c009ba1b
Damian Kulp <damiankulp@gmail.com>
2022-12-30 17:57:33 -0500

    remove old stuff

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,28 @@ var sourcemaps = require("gulp-sourcemaps");
 var browserSync = require("browser-sync").create();
 
 gulp.task("ts", function () {
-	return gulp.src("client2/ts/*.ts")
+	return gulp.src("client/ts/*.ts")
 		.pipe(sourcemaps.init())
 		.pipe(ts("./tsconfig.json"))
-		.pipe(sourcemaps.write(".", {sourceRoot: "client2/ts"}))
-		.pipe(gulp.dest("client2/ts"))
+		.pipe(sourcemaps.write(".", {sourceRoot: "client/ts"}))
+		.pipe(gulp.dest("client/ts"))
 		.pipe(browserSync.stream());
 });
 
 gulp.task("html", function() {
-	return gulp.src("client2/*.html")
+	return gulp.src("client/*.html")
 		.pipe(browserSync.stream());
 })
 
 gulp.task("watch", function(cb) {
-	gulp.watch(["client2/ts/*.ts"], gulp.series("ts"));
-	gulp.watch(["client2/*.html"], gulp.series("html"));
+	gulp.watch(["client/ts/*.ts"], gulp.series("ts"));
+	gulp.watch(["client/*.html"], gulp.series("html"));
 	cb();
 });
 
 gulp.task("start", function() {
 	browserSync.init({
-		server: "./client2/"
+		server: "./client/"
 	});
 });
 
